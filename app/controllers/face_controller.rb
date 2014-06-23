7f8c3da4ce52519ae9b15040b31d76358e4a32d6
tigawa <ihciiat@gmail.com>
2014-06-24 08:11:15 +0900

    yahoo Webapiを呼び出したときのサンプルを貼り付けた

--- app/controllers/face_controller.rb
+++ app/controllers/face_controller.rb
@@ -1,23 +1,24 @@
 class FaceController < ApplicationController
-  def index
-  end
+	require 'nokogiri'
 
-  def parse
-  	#TODO: ここに顔を解析して座標を取得する処理を記載する。　とりあえずYahooのAPIを呼び出し時のサンプルを貼り付ける。
-  	conn = Faraday.new(:url => 'http://jlp.yahooapis.jp') do |builder|
-  	    builder.request :url_encoded
-  	    builder.response :logger
-  	    builder.adapter :net_http
+	def index
+	end
+
+	def parse
+	  	#TODO: ここに顔を解析して座標を取得する処理を記載する。　とりあえずYahooのAPIを呼び出し時のサンプルを貼り付ける。
+	  	conn = Faraday.new(:url => 'http://jlp.yahooapis.jp') do |builder|
+	  		builder.request :url_encoded
+	  		builder.response :logger
+	  		builder.adapter :net_http
+	  	end
+	  	res = conn.get '/KouseiService/V1/kousei', {
+	  		:appid => "dj0zaiZpPTxxxxxxxxxxxxxxxxxxxxxxxxxxxxWVyc2VjcmV0Jng9NDI-",
+	  		:sentence => sentence,
+	  	}
+	  	Nokogiri::XML(res.body)
+	end
+	
+	def speak
+  		#TODO: ここに文章を送ってmp3に変換して返すメソッドを記載する。
   	end
-
-  	res = conn.get '/KouseiService/V1/kousei', {
-  	    :appid => "dj0zaiZpPTxxxxxxxxxxxxxxxxxxxxxxxxxxxxWVyc2VjcmV0Jng9NDI-",
-  	    :sentence => sentence,
-  	}
-  	Nokogiri::XML(res.body)
-  end
-
-  def speak
-  	#TODO: ここに文章を送ってmp3に変換して返すメソッドを記載する。
-  end
 end
