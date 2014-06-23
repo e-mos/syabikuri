caef77d12e2c9b8e23f93ebafa616ee5d07a6f9c
tigawa <ihciiat@gmail.com>
2014-06-24 07:25:10 +0900

    yahoo Webapiを呼び出したときのサンプルを貼り付けた

--- app/controllers/face_controller.rb
+++ app/controllers/face_controller.rb
@@ -3,7 +3,18 @@ class FaceController < ApplicationController
   end
 
   def parse
-  	#TODO: ここに顔を解析して座標を取得する処理を記載する。
+  	#TODO: ここに顔を解析して座標を取得する処理を記載する。　とりあえずYahooのAPIを呼び出し時のサンプルを貼り付ける。
+  	conn = Faraday.new(:url => 'http://jlp.yahooapis.jp') do |builder|
+  	    builder.request :url_encoded
+  	    builder.response :logger
+  	    builder.adapter :net_http
+  	end
+
+  	res = conn.get '/KouseiService/V1/kousei', {
+  	    :appid => "dj0zaiZpPTxxxxxxxxxxxxxxxxxxxxxxxxxxxxWVyc2VjcmV0Jng9NDI-",
+  	    :sentence => sentence,
+  	}
+  	Nokogiri::XML(res.body)
   end
 
   def speak
